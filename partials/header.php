a950075bddc2acfe3b848c56821bee6a4a0ddc19
PeterGanoczi <peter.ganoczi@gmail.com>
2019-09-26 22:36:29 +0200

    head nav + new css file

--- partials/header.php
+++ partials/header.php
@@ -5,6 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="client/assets/forms.css">
+    <link rel="stylesheet" href="client/assets/main.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <title>WebDiy</title>
 </head>
 <body>    
+    <header>
+        <ul class="leftnav">
+            <li>Home</li>
+        </ul>
+
+        <input type="text">
+
+        <ul class="rightnav">
+            <li>Log in</li>
+            <li>Sign up</li>
+        </ul>
+    </header>
